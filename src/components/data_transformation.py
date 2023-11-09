e41805a08dfb2e1cd5075b6c572858e25108c81d
alexaaronruban <alexaaron12@gmail.com>
2023-11-09 21:18:09 +0000

    Commit

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -24,17 +24,17 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function si responsible for data trnasformation
+        This function is responsible for data transformation
         
         '''
         try:
-            numerical_columns = ["writing_score", "reading_score"]
+            numerical_columns = ["math_score", "reading_score"]
             categorical_columns = [
                 "gender",
                 "race_ethnicity",
                 "parental_level_of_education",
                 "lunch",
-                "test_preparation_course",
+                "test_preparation_course"
             ]
 
             num_pipeline= Pipeline(
@@ -85,8 +85,8 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
+            target_column_name="writing_score"
+            numerical_columns = ["math_score", "reading_score"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
